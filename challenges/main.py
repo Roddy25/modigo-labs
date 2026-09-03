48dfaa0d3f6a08c5dead1c0ab57bf0733e24eef4
Roddy25 <101779384+Roddy25@users.noreply.github.com>
2026-09-03 09:07:40 +0100

    Complete: Remove Duplicates

--- challenges/main.py
+++ challenges/main.py
@@ -1,11 +1,6 @@
-def build_countdown(start):
-    if start == 0:
-        return []
-    countdown = []
-    for i in range(start, 0, -1):
-        countdown.append(i)
-    return countdown
-print (build_countdown(5))
-print(build_countdown(1))
-print(build_countdown(0))
-print(build_countdown(3))
+def remove_duplicates(items):
+    duplicates = []
+    for item in items:
+        if item not in duplicates:
+            duplicates.append(item)
+    return duplicates
