ab54ea89a0c0d3d594f5c68ced18a01a7c4a4821
Roddy25 <101779384+Roddy25@users.noreply.github.com>
2026-09-03 10:09:29 +0100

    Complete: BMI Category Report

--- challenges/main.py
+++ challenges/main.py
@@ -1,6 +1,15 @@
-def sum_of_digits(number):
-    # TODO: use a for loop to add up each digit of `number`
-    total = 0
-    for num in str(number):
-        total += int(num)
-    return total
+def bmi_report(weight_kg, height_m):
+    # TODO: calculate bmi, round it to 1 decimal place, determine the category,
+    # and return "BMI: {bmi}, Category: {category}"
+    bmi = round(weight_kg / (height_m ** 2), 1)
+    category = ""
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi >= 18.5 and bmi <= 24.9:
+        category = "Normal weight"
+    elif bmi >= 25.0 and bmi <= 29.9:
+        category = "Overweight"
+    elif bmi >= 30.0:
+        category = "Obese"
+    return f"BMI: {bmi}, Category: {category}"
+print(bmi_report(50, 1.70))
