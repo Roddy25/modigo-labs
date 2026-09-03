da559965b29cc7185c8563fbefa2edadd609a61a
Roddy25 <101779384+Roddy25@users.noreply.github.com>
2026-09-03 10:47:37 +0100

    Complete: Title Case Converter

--- challenges/main.py
+++ challenges/main.py
@@ -1,6 +1,15 @@
-def double_values(numbers):
-    double = []
-    for i in numbers:
-        double.append(i * 2)
-    return double
-print(double_values([1, 2, 3]))
+def smart_title_case(sentence):
+    # TODO: capitalize each word except connector words (a, an, the, of, in, on, and),
+    # unless that connector word is the first word in the sentence
+    if not sentence:
+        return ""
+    small_words = {"a", "an", "the", "of", "in", "on", "and"}
+    words = sentence.split()
+    result = []
+    for i, word in enumerate(words):
+        if i == 0 or word not in small_words:
+            result.append(word.capitalize())
+        else:
+            result.append(word)
+    return " ".join(result)
+print(smart_title_case("the lord of the rings"))
