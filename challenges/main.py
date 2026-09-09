d791b7a22909e8bdb10b32cf6c7594afb55e70cf
Roddy25 <101779384+Roddy25@users.noreply.github.com>
2026-09-09 08:50:01 +0100

    Complete: Rectangle Area Finder

--- challenges/main.py
+++ challenges/main.py
@@ -1,10 +1,6 @@
-def calculate_total(price, quantity, delivery_fee):
-    """
-    Calculates the total cost of an order: (price * quantity) + delivery_fee.
-    """
-    subtotal = price * quantity  # already done for you
+length = int(input("Length: "))
+width = int(input("Width: "))
 
-    # TODO: create a variable called `total` that adds delivery_fee to subtotal
-    total = delivery_fee + subtotal # TODO: replace 0 with the correct calculation
+area = length * width
 
-    return total
+print(f"Area: {area}")
